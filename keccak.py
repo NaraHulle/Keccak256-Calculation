36348c0839029258bc90803ebcc8d390a0a0a597
Nara Hulle <122056953+NaraHulle@users.noreply.github.com>
2023-08-20 11:43:22 +0300

    Update keccak.py

--- keccak.py
+++ keccak.py
@@ -2,7 +2,7 @@ from Crypto.Hash import keccak
 
 hex_code = '213d031b8d8604d6beca465584de588430badf68624eb9791dabe41ebd765bdc' #your hex code
 
-# Convert hex code to bytes::
+# Convert hex code to bytes
 message = bytes.fromhex(hex_code)
 
 # Calculate Keccak-256 hash:
